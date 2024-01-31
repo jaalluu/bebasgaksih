e2c3321f62998b684c0834b0e4e5bad4a0dbb189
Jalu <piawaitrisulo@MacBook-Air-Jalu.local>
2024-01-31 09:15:56 +0700

    tes ah

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hahalolo" }');
+    res.send('{ "response": "Huh" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hola" }');
+    res.send('{ "response": "Hah" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": "Helo" }');
+    res.send('{ "response": "Heh" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
