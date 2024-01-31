2f9bfdf8bffd55de22f1e17cff65623f6e2ee9b4
Jalu <piawaitrisulo@MacBook-Air-Jalu.local>
2024-01-31 09:03:03 +0700

    ngetes nih

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Me" }');
+    res.send('{ "response": "Halo" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Hola" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('{ "response": "Helo" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
