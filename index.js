835b029cf03415bfcfb18d84f84d362ec3137717
Jalu <piawaitrisulo@MacBook-Air-Jalu.local>
2024-02-01 14:55:42 +0700

    AWTB-5 cobacoba

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Huh" }');
+    res.send('{ "response": "Huhuu" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hah" }');
+    res.send('{ "response": "Hahaa" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": "Heh" }');
+    res.send('{ "response": "Hehee" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
