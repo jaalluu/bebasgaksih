22dc3bad0720cd025dc809b11bd4a551e4165fc5
Jalu <piawaitrisulo@MacBook-Air-Jalu.local>
2024-02-01 16:08:15 +0700

    testing nih

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Huhuu" }');
+    res.send('{ "response": "hadoh" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hahaa" }');
+    res.send('{ "response": "hadooh" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": "Hehee" }');
+    res.send('{ "response": "hadoooh" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
